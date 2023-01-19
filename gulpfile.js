87335e325607954c7c401c171478ea1b8398ee2d
Nail Markov <markov_nail@mail.ru>
2023-01-19 23:26:55 +0300

    Добавляет ДЗ#8

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 import gulp from 'gulp';
+import { deleteSync as clean } from 'del';
 import getData from 'gulp-data';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import minifyCss from 'postcss-csso';
+import rename from 'gulp-rename';
 import posthtml from 'gulp-posthtml';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import minifyJs from 'gulp-terser';
 import twig from 'gulp-twig';
 import sortMediaQueries from 'postcss-sort-media-queries';
 import { stacksvg } from 'gulp-stacksvg';
+import svgo from 'gulp-svgo';
+import createWebp from 'gulp-webp';
+import optimizeImages from 'gulp-imagemin';
+import optimizeJpeg from 'imagemin-mozjpeg';
+import optimizePng from 'imagemin-pngquant';
+import optimizeSvg from 'imagemin-svgo';
+import svgoConfig from './svgo.config.js';
+import useCondition from 'gulp-if';
+
 
 // Styles
 
@@ -18,11 +31,15 @@ export const styles = () => {
     .pipe(less())
     .pipe(postcss([
       sortMediaQueries(),
-      autoprefixer()
+      autoprefixer(),
+      minifyCss()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
 
 const buildHtml = () => {
   return gulp
@@ -40,13 +57,74 @@ const buildHtml = () => {
     )
     .pipe(twig())
     .pipe(posthtml())
-    .pipe(gulp.dest('source'));
+    .pipe(gulp.dest('build'));
 }
+
+// Scripts
+
+const buildScripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(minifyJs())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('build/js'));
+}
+
+// Images
+
+const isDev = process.argv.includes('--dev');
+
+const buildImages = () => {
+  return gulp
+    .src('source/img/**/*.{jpg,png,svg}')
+    .pipe(
+      useCondition(
+        isDev,
+        optimizeImages([
+          optimizePng(),
+          optimizeJpeg({ progressive: true, quality: 75 }),
+          optimizeSvg(svgoConfig),
+        ])
+      )
+    )
+    .pipe(gulp.dest('build/img'))
+    .pipe(createWebp({ quality: 75 }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// Sprite
 
 const buildSprite = () => {
   return gulp.src('source/img/icon/**/*.svg')
     .pipe(stacksvg({ output: 'sprite' }))
-    .pipe(gulp.dest('source/img'));
+    .pipe(gulp.dest('build/img'));
+}
+
+// SVG
+
+const buildSvgo = () => {
+  return gulp.src('source/img/*.svg')
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+}
+
+// Copy
+
+const buildCopy = (done) => {
+  gulp.src([
+    'source/static',
+    'source/*.{ico,manifest}',
+  ], {
+    base: 'source'
+  })
+  .pipe(gulp.dest('build'))
+  done();
+}
+
+// Delete
+
+const buildDelete = (done) => {
+  clean('build');
+  done();
 }
 
 const reload = (done) => {
@@ -77,6 +155,20 @@ const watcher = () => {
   gulp.watch('source/img/icon/**/*.svg', gulp.series(buildSprite, reload));
 }
 
+// Build
+
+export const build = gulp.series(
+  buildDelete,
+  buildCopy,
+  buildImages,
+  gulp.parallel(
+    styles,
+    buildHtml,
+    buildScripts,
+    buildSprite,
+    buildSvgo
+  ),
+);
 
 export default gulp.series(
   buildHtml, buildSprite, styles, server, watcher
