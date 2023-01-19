c6bee7e129e745a47aa0722276aa0ea7a30d3271
Nail Markov <markov_nail@mail.ru>
2023-01-19 23:38:56 +0300

    Правка файла

--- svgo.config.js
+++ svgo.config.js
@@ -1,26 +1,26 @@
 export default {
-	multipass: true,
-	plugins: [
-		...[
-			'removeDoctype',
-			'removeXMLProcInst',
-			'removeEditorsNSData',
-			'removeMetadata',
-			'removeComments',
-			'removeDesc',
-			'removeTitle',
-			'removeUselessDefs',
-			'removeEmptyAttrs'
-		].map((name) => ({
-			active: true,
-			name
-		})),
-		...['cleanupNumericValues', 'convertPathData', 'convertTransform', 'cleanupListOfValues'].map((name) => ({
-			active: true,
-			name,
-			params: {
-				floatPrecision: 2
-			}
-		}))
-	]
+  multipass: true,
+    plugins: [
+      ...[
+        'removeDoctype',
+        'removeXMLProcInst',
+        'removeEditorsNSData',
+        'removeMetadata',
+        'removeComments',
+        'removeDesc',
+        'removeTitle',
+        'removeUselessDefs',
+        'removeEmptyAttrs'
+      ].map((name) => ({
+        active: true,
+        name
+      })),
+      ...['cleanupNumericValues', 'convertPathData', 'convertTransform', 'cleanupListOfValues'].map((name) => ({
+        active: true,
+        name,
+        params: {
+        floatPrecision: 2
+      }
+    }))
+  ]
 };
